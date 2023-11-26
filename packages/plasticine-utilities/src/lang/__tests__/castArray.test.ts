9820985ab2f701efd684100edbdf562352ad6761
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-11-26 22:59:03 +0800

    fix: complete undefined case

--- packages/plasticine-utilities/src/lang/__tests__/castArray.test.ts
+++ packages/plasticine-utilities/src/lang/__tests__/castArray.test.ts
@@ -2,7 +2,7 @@ import { castArray } from '../castArray'
 
 describe('castArray', () => {
   test('should wrap non-array items in an array', () => {
-    const dataList: any[] = [1, 'a', true, { name: 'foo' }]
+    const dataList: any[] = [1, 'a', true, { name: 'foo' }, undefined, null]
     const expected = dataList.map((data) => [data])
     const result = dataList.map(castArray)
 
--- packages/plasticine-utilities/src/lang/castArray.ts
+++ packages/plasticine-utilities/src/lang/castArray.ts
@@ -1,5 +1,15 @@
-export function castArray<T>(itemOrArray?: T | T[]): T[] {
-  if (itemOrArray === undefined) {
+/**
+ * Casts values as array if it's not none.
+ * @param itemOrArray value or array
+ * @returns cast array
+ *
+ * @example
+ * castArray(1) === [1]
+ * castArray([1]) === [1]
+ * castArray(undefined) === [undefined]
+ */
+export function castArray<T>(itemOrArray?: T | undefined | T[] | undefined[]): (T | undefined)[] {
+  if (arguments.length === 0) {
     return []
   }
 
